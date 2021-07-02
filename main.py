2dfcf2548833a43f2ae75f0c618a762ee13e52f6
SillyGithub5 <84869156+SillyGithub5@users.noreply.github.com>
2021-07-02 10:02:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import random
+import time
+import playsound
+from соdе import CAH
+CAH()
+#Dear whoever is stealing my code right now i have in fact hidden my code somewhere you shall never find it so have fun how i dont .env you
